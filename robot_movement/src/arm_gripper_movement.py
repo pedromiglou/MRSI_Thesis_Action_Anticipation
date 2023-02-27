de29fd92c28f300c619ad027aaaef02b277ec1a5
Pedro Amaral <pedromiglou@hotmail.com>
2023-02-27 14:40:37 +0000

    arm_gripper_movement improvements

--- robot_movement/src/arm_gripper_movement.py
+++ robot_movement/src/arm_gripper_movement.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 import os
+import sys
 
 #from config.definitions import ROOT_DIR
 from larcc_classes.ur10e_control.ArmGripperComm import ArmGripperComm
@@ -17,24 +18,6 @@ args = vars(parser.parse_args())
 # path = ROOT_DIR + "/use_cases/config/"
 path = "/home/miglou/catkin_ws/src/MRSI_Thesis/robot_movement/config/"
 
-if args['movement'] == "":
-    res = os.listdir(path)
-    i = 0
-
-    for file in res:
-        print(f'[{i}]:' + file)
-        i += 1
-
-    idx = input("Select idx from test json: ")
-
-    f = open(path + res[int(idx)])
-    config = json.load(f)
-    f.close()
-else:
-    f = open(path + args["movement"] + '.json')
-    config = json.load(f)
-    f.close()
-
 rospy.init_node("arm_gripper_movement", anonymous=True)
 
 arm_gripper_comm = ArmGripperComm()
@@ -46,12 +29,46 @@ time.sleep(0.2)
 # if not arm_gripper_comm.state_dic["gripper_active"]:
 #     arm_gripper_comm.gripper_init()
 
-for pos in config["positions"]:
-    arm_gripper_comm.move_arm_to_joints_state(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
+if args['movement'] == "":
+    res = os.listdir(path)
 
-    # if pos[6] == 1:
-    #     arm_gripper_comm.gripper_open_fast()
-    # if pos[6] == -1:
-    #     arm_gripper_comm.gripper_close_fast()
+    while True:
+        i = 0
+
+        for file in res:
+            print(f'[{i}]:' + file)
+            i += 1
+
+        idx = input("Select idx from test json: ")
+
+        try:
+            f = open(path + res[int(idx)])
+            config = json.load(f)
+            f.close()
+        
+        except:
+            # arm_gripper_comm.gripper_disconnect()
+            sys.exit(0)
+
+        for pos in config["positions"]:
+            arm_gripper_comm.move_arm_to_joints_state(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
+
+            # if pos[6] == 1:
+            #     arm_gripper_comm.gripper_open_fast()
+            # if pos[6] == -1:
+            #     arm_gripper_comm.gripper_close_fast()
+
+else:
+    f = open(path + args["movement"] + '.json')
+    config = json.load(f)
+    f.close()
+
+    for pos in config["positions"]:
+        arm_gripper_comm.move_arm_to_joints_state(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
+
+        # if pos[6] == 1:
+        #     arm_gripper_comm.gripper_open_fast()
+        # if pos[6] == -1:
+        #     arm_gripper_comm.gripper_close_fast()
 
 # arm_gripper_comm.gripper_disconnect()
