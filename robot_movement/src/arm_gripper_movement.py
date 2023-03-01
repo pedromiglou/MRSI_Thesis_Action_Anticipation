e8505d308e4263e86427bef904e4df6e5d6ece1f
Pedro Amaral <pedromiglou@hotmail.com>
2023-03-01 18:46:50 +0000

    better way to configure movements

--- robot_movement/src/arm_gripper_movement.py
+++ robot_movement/src/arm_gripper_movement.py
@@ -31,6 +31,11 @@ arm_gripper_comm.gripper_connect()
 if not arm_gripper_comm.state_dic["activation_completed"]: 
     arm_gripper_comm.gripper_init()
 
+f = open(path + "positions.json")
+positions = json.load(f)
+positions = positions["positions"]
+f.close()
+
 if args['movement'] == "":
     res = os.listdir(path)
 
@@ -52,12 +57,13 @@ if args['movement'] == "":
             arm_gripper_comm.gripper_disconnect()
             sys.exit(0)
 
-        for pos in config["positions"]:
+        for pos, gripper in config["positions"]:
+            pos = positions[pos]
             arm_gripper_comm.move_arm_to_joints_state(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
 
-            if pos[6] == 1:
+            if gripper == 1:
                 arm_gripper_comm.gripper_open_fast()
-            if pos[6] == -1:
+            if gripper == -1:
                 arm_gripper_comm.gripper_close_fast()
 
 else:
@@ -65,17 +71,13 @@ else:
     config = json.load(f)
     f.close()
 
-    for pos in config["positions"]:
+    for pos, gripper in config["positions"]:
+        pos = positions[pos]
         arm_gripper_comm.move_arm_to_joints_state(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
 
-        if pos[6] == 1:
+        if gripper == 1:
             arm_gripper_comm.gripper_open_fast()
-        if pos[6] == -1:
+        if gripper == -1:
             arm_gripper_comm.gripper_close_fast()
 
 arm_gripper_comm.gripper_disconnect()
-
-# positions down already switched:
-# 4R -> [-0.7381933371173304, -0.9487016958049317, 1.5844123999225062, -2.2606059513487757, -1.5595677534686487, 0.8322415351867676]
-# 4G -> [-0.6696541945086878, -0.7871526044658204, 1.2948415915118616, -2.1347090206541957, -1.549922291432516, 0.9193868637084961]
-# 2G -> [-0.6016314665423792, -0.5837524694255372, 0.8884509245501917, -1.874162336389059, -1.550450627003805, 0.9975481033325195]
