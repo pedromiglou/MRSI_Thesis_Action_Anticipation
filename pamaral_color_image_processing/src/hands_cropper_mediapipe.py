23e75c0167ca20071dc5d550e08a21f3cfea9cfa
Pedro Amaral <pedromiglou@hotmail.com>
2023-05-29 11:42:38 +0100

    fix RAM issues with mediapipe

---
+++ pamaral_color_image_processing/src/hands_cropper_mediapipe.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import cv2
+import rospy
+import mediapipe as mp
+
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+from mediapipe_utils import find_hands
+
+
+class HandsCropperMediapipe:
+
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.subscriber = rospy.Subscriber("/hand_usb_cam/image_raw", Image, self.image_callback)
+        self.hand_image_publisher = rospy.Publisher("/hand_image", Image, queue_size=1)
+
+        # Initialize MediaPipe Hands
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+
+    def image_callback(self, msg):
+        try:
+            img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        except:
+            print("Error reading color image")
+            return
+
+        # Process the image with Mediapipe Hands
+        results = self.hands.process(img_rgb)
+
+        # Draw hand landmarks on the frame
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+
+        self.hand_image_publisher.publish(self.bridge.cv2_to_imgmsg(img))
+        """
+        mp_data = {}
+        mp_data["face_points"] = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        mp_data["threshold_points"] = (11, 12, 24, 25) # shoulders and hips
+        mp_data["left_hand_points"] = (16, 18, 20, 22)
+        mp_data["right_hand_points"] = (15, 17, 19, 21)
+        mp_data["mp_drawing"] = mp.solutions.drawing_utils
+        mp_data["mp_drawing_styles"] = mp.solutions.drawing_styles
+        mp_data["mp_pose"] = mp.solutions.pose
+        mp_data["pose"] = mp_data["mp_pose"].Pose(static_image_mode=False,
+                                                model_complexity=2,
+                                                enable_segmentation=False,
+                                                min_detection_confidence=0.7)
+        
+        left_bounding, right_bounding, hand_right, hand_left, mp_data["pose"] = find_hands(
+                img, mp_data, x_lim=15, y_lim=15)
+
+        if hand_right is not None:
+            img = cv2.cvtColor(hand_right, cv2.COLOR_RGB2BGR)
+            self.hand_image_publisher.publish(self.bridge.cv2_to_imgmsg(hand_right))
+        """
+
+
+def main():
+    # ---------------------------------------------------
+    # INITIALIZATION
+    # ---------------------------------------------------
+    default_node_name = 'hands_cropper_mediapipe'
+    rospy.init_node(default_node_name, anonymous=False)
+
+    HandsCropperMediapipe()
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
