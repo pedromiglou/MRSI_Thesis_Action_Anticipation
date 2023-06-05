98f024cd6df6ce679cfd2df338e00cd8cf033adf
Pedro amaral <pedromiglou@hotmail.com>
2023-06-05 16:18:57 +0100

    some fixes

--- pamaral_utils/src/video_recorder.py
+++ pamaral_utils/src/video_recorder.py
@@ -9,10 +9,11 @@ from datetime import datetime
 
 
 class VideoRecorder:
-    def __init__(self, input_topic, filename):
+    def __init__(self, input_topic, filename, fps):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(input_topic, Image, self.image_callback)
         self.filename = filename
+        self.fps = fps
         self.video_writer = None
         self.is_recording = False
 
@@ -39,7 +40,7 @@ class VideoRecorder:
         if not self.is_recording:
             # Create video writer object
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            self.video_writer = cv2.VideoWriter(self.filename, fourcc, 10.0, (640, 480))
+            self.video_writer = cv2.VideoWriter(self.filename, fourcc, self.fps, (640, 480))
 
             # Set flag to start recording
             self.is_recording = True
@@ -69,12 +70,13 @@ def main():
 
     input_topic = rospy.get_param(rospy.search_param('input_image_topic'))
     output_folder = rospy.get_param(rospy.search_param('output_video_folder'))
+    fps = rospy.get_param(rospy.search_param('fps'))
 
     # Create a new folder
     now = datetime.now()
     filename = output_folder + now.strftime("%d_%m_%Y_%H:%M:%S")+ ".mp4"
 
-    VideoRecorder(input_topic=input_topic, filename=filename)
+    VideoRecorder(input_topic=input_topic, filename=filename, fps=float(fps))
 
     rospy.spin()
 
