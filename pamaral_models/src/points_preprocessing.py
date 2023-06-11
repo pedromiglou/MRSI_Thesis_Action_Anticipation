e39430c0d0e03ddde17453181efa9927e067b590
Pedro Amaral <pedromiglou@hotmail.com>
2023-06-11 21:24:36 +0100

    record_dataset.launch

--- pamaral_models/src/points_preprocessing.py
+++ pamaral_models/src/points_preprocessing.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import rospy
 
+from cv_bridge import CvBridge
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import Image
 
@@ -13,6 +14,7 @@ from pamaral_models.msg import PointList
 class PointsPreprocessing:
 
     def __init__(self):
+        self.bridge = CvBridge()
         self.mp_points_image_publisher = rospy.Publisher("mp_points_image", Image, queue_size=1)
         self.preprocessed_points_publisher = rospy.Publisher("preprocessed_points", PointList, queue_size=1)
         self.mp_points_sub = rospy.Subscriber("mp_points", PointList, self.mp_points_callback)
@@ -31,7 +33,7 @@ class PointsPreprocessing:
 
         points = (points/max_value + 1)/2
 
-        mp_points_image = np.zeros((480, 640))
+        mp_points_image = np.zeros((480, 640, 3), dtype=np.uint8)
 
         # Draw the points on the image
         point_radius = 5
@@ -44,7 +46,7 @@ class PointsPreprocessing:
 
         msg = PointList()
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = self.input_topic
+        msg.header.frame_id = "mp_points"
         msg.points = [Point(p[0], p[1], p[2]) for p in points]
         self.preprocessed_points_publisher.publish(msg)
 
