94f3b5a75db117caa4817195a2f78e12b24c5e23
Pedro Amaral <pedromiglou@hotmail.com>
2023-06-15 10:22:56 +0100

    launch record_dataset.py

--- pamaral_models/src/record_dataset.py
+++ pamaral_models/src/record_dataset.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import rospy
+import subprocess
 import time
 
 from std_srvs.srv import Empty
@@ -9,6 +12,9 @@ rospy.init_node('service_client_node')
 # Wait for the service to become available
 rospy.wait_for_service('/front_camera/start_capture')
 rospy.wait_for_service('/front_camera/stop_capture')
+
+rosbag_cmd = ['rosbag', 'record', 'front_camera/color/image_raw']
+rosbag_process = subprocess.Popen(rosbag_cmd)
 
 for i in range(3,0,-1):
     print(f"Starting in {i}")
@@ -25,5 +31,7 @@ for i in range(30,0,-1):
     time.sleep(1)
 
 service_proxy = rospy.ServiceProxy('/front_camera/stop_capture', Empty)
+
+rosbag_process.terminate()
 
 service_proxy()
