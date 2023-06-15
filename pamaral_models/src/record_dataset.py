14cddb91be23168e66cec7a8be181cfcd7567359
Pedro Amaral <pedromiglou@hotmail.com>
2023-06-15 11:41:33 +0100

    custom names for bag files

--- pamaral_models/src/record_dataset.py
+++ pamaral_models/src/record_dataset.py
@@ -4,6 +4,7 @@ import rospy
 import subprocess
 import time
 
+from datetime import datetime
 from std_srvs.srv import Empty
 
 # Initialize the ROS node
@@ -13,7 +14,25 @@ rospy.init_node('service_client_node')
 rospy.wait_for_service('/front_camera/start_capture')
 rospy.wait_for_service('/front_camera/stop_capture')
 
-rosbag_cmd = ['rosbag', 'record', 'front_camera/color/image_raw']
+output_folder = rospy.get_param(rospy.search_param('output_folder'))
+
+people = ["pedro", "joel", "manuel"]
+for i, p in enumerate(people):
+    print(f"{i} - {p}")
+
+person = int(input("?"))
+
+objects = ["ball", "cube", "bottle", "phone", "plier", "screwdriver", "wood_block"]
+for i, o in enumerate(objects):
+    print(f"{i} - {o}")
+
+object = int(input("?"))
+
+t = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
+filename = f"{output_folder}/{objects[object]}_{people[person]}_{t}.bag"
+
+rosbag_cmd = ['rosbag', 'record', '-j', '-O', filename, 'front_camera/color/image_raw']
 rosbag_process = subprocess.Popen(rosbag_cmd)
 
 for i in range(3,0,-1):
